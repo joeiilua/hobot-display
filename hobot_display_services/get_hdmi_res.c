c7061c6327bb5bc629d098c5aeaf9e89407fd7ec
luojiale <jiale.luo@horizon.ai>
2023-12-08 18:01:41 +0800

    feat: Add default resolution while edid is null
    Summary:
    	1. While screen edid is null,the following resolutions will be added:
    	1920x1080_30 1920x1080_60 1280x720_60 1280x720_30 1024x768_60 1024x768_30 800x600_60.32 640x480_75
    	2. Fix get_hdmi_res only get first resolution on edid
    
    Signed-off-by: luojiale <jiale.luo@horizon.ai>

--- hobot_display_services/get_hdmi_res.c
+++ hobot_display_services/get_hdmi_res.c
@@ -1,55 +1,30 @@
-#include <assert.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <getopt.h>
-#include <malloc.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include "lt8618_ioctl.h"
-#include <argp.h>
-#include <asm/types.h>
-#include <dlfcn.h>
-#include <linux/fb.h>
-#include <pthread.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include "hb_vot.h"
-// #include "iar_interface.h"
-#include "iar_interface.h"
-#include "hb_vio_interface.h"
-#include <signal.h>
+#include <linux/fb.h>
 
-int main(int argc, char **argv)
-{
-
-    hdmi_timing_t iar_timing = {0};
-
-    hdmi_timing_t hdmi_timing;
-    int ret = 0;
-    lt8618_ioctl_init();
-    memset(&hdmi_timing, 0, sizeof(hdmi_timing));
-    ret = lt8618_get_edid_resolution_ratio((hdmi_timing_t *)&hdmi_timing);
-    // LOG("main ret = %d, data.hfp = %d,data.hs=%d,data.hbp = %d,data.hact=%d,data.htotal = %d,data.vfp=%d,data.vs = %d,data.vbp=%d,data.vact=%d,data.vtotal=%d,data.vic=%d,data.pic_ratio=%d,data.clk=%d\n",
-    //     ret, hdmi_timing.hfp, hdmi_timing.hs, hdmi_timing.hbp, hdmi_timing.hact, hdmi_timing.htotal, hdmi_timing.vfp, hdmi_timing.vs, hdmi_timing.vbp, hdmi_timing.vact, hdmi_timing.vtotal, hdmi_timing.vic, hdmi_timing.pic_ratio, hdmi_timing.clk);
-    if (!ret)
-    {
-        if (hdmi_timing.vact > 1080)
-        { /*iar only support 1080p*/
-            hdmi_timing.vact = 1080;
-            hdmi_timing.hact = 1920;
-        }
-        printf("%d,%d", hdmi_timing.vact, hdmi_timing.hact);
+int main() {
+    // open fb device
+    int fb = open("/dev/fb0", O_RDWR);
+    if (fb == -1) {
+        perror("Can't open fb device");
+        exit(EXIT_FAILURE);
     }
-    else
-    {
-        printf("1080,1920");
+
+    // get fb info
+    struct fb_var_screeninfo vinfo;
+    if (ioctl(fb, FBIOGET_VSCREENINFO, &vinfo)) {
+        perror("Can't get fb info");
+        close(fb);
+        exit(EXIT_FAILURE);
     }
+    printf("%d,%d", vinfo.yres, vinfo.xres);
+
+    // close
+    close(fb);
 
     return 0;
 }
+
